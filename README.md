e6af6981b183a6c3a78eeba8b1e2981b298f9dd5
Marcus Nyeholt <nyeholt@gmail.com>
2014-10-21 18:52:38 +1100

    Updated cdncontent to handle the fact that SS treats non-local URLs as '[embed]' shortcode items, thus not applying the width/height options. There's a better solution, but this is required in the immediate timeframe.

--- README.md
+++ README.md
@@ -15,3 +15,12 @@ Provides a few CDN related pieces of functionality
 
 * Content Services module https://github.com/nyeholt/silverstripe-content-services/
 * Patches to the framework folder - see gist at https://gist.github.com/nyeholt/8642905
+
+## Installation
+
+* In your local configuration, add 
+
+  ShortcodeParser::get('default')->register('embed', array('CdnEmbedder', 'handle\_shortcode'));
+
+to ensure the cdn is manipulated for resized images because SS treats CDN sourced content
+as an embed tag. This may be addressed in a future commit
---
+++ code/oembed/CdnEmbedder.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * 
+ *
+ * @author <marcus@silverstripe.com.au>
+ * @license BSD License http://www.silverstripe.org/bsd-license
+ */
+class CdnEmbedder extends Oembed {
+	public static function handle_shortcode($arguments, $url, $parser, $shortcode) {
+		// get all registered cdn things, and see if any of them have URLs that 
+		// need to be handled by US, instead of looked up. _if_ we need to handle it,
+		// perform relevant transforms etc. 
+		$contentService = singleton('ContentService');
+		/* @var $contentService ContentService */
+		$cdns = $contentService->getStoreTypes();
+		
+		foreach ($cdns as $name => $types) {
+			$reader = $contentService->getReader($name);
+			if ($actualReader = $reader->providerOfUrl($url)) {
+				if ($actualReader instanceof ContentReader) {
+					$contentId = $actualReader->getContentId();
+					$file = File::get()->filter('CDNFile', $contentId)->first();
+					
+					if ($file) {
+						if ($file instanceof Image && isset($arguments['width']) && isset($arguments['height'])) {
+							// return the formatted image
+							$cached = $file->ResizedImage($arguments['width'], $arguments['height']);
+							if ($cached) {
+								return $cached->forTemplate();
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		return parent::handle_shortcode($arguments, $url, $parser, $shortcode);
+	}
+}

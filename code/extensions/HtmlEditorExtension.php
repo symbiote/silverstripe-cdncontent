371c6205966d3eae025e9261f3ecdcf02b3e024d
Marcus Nyeholt <marcus@silverstripe.com.au>
2016-03-01 17:14:49 +1100

    FIX Ensure an image object is present before referencing it

--- code/extensions/HtmlEditorExtension.php
+++ code/extensions/HtmlEditorExtension.php
@@ -5,9 +5,13 @@
  */
 class HtmlEditorExtension extends Extension {
     public function processImage($image, $img) {
+        if (!$image) {
+            return;
+        }
         if ($image->CanViewType && $image->getViewType() != 'Anyone') {
             return;
         }
+        
         $img->setAttribute('data-cdnfileid', $image->ID);
     }
 }

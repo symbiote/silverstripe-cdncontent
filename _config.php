3404a0f9016a39aeb09194b7df381ef6fc179473
jcarter <josh@symbiote.com.au>
2020-06-01 12:24:06 +1000

    add(7.4): Support for PHP 7.4

--- _config.php
+++ _config.php
@@ -2,5 +2,5 @@
 
 Image::set_backend('Symbiote\\ContentServiceAssets\\ImageBackend');
 
-//Object::add_extension('Folder', 'CDNFolder');
-//Object::add_extension('File', 'CDNFile');
+//SS_Object::add_extension('Folder', 'CDNFolder');
+//SS_Object::add_extension('File', 'CDNFile');

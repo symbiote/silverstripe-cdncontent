9da231f112a3cd1cdc1defdbdd37552c1e3e5d5b
jcarter <josh@symbiote.com.au>
2020-06-04 09:13:10 +1000

    chore(7.4): Replace Object with SS_Object

--- _config.php
+++ _config.php
@@ -1,5 +1,9 @@
 <?php
 
+// Ensure compatibility with PHP 7.2 ("object" is a reserved word),
+// with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
+
 Image::set_backend('Symbiote\\ContentServiceAssets\\ImageBackend');
 
 //SS_Object::add_extension('Folder', 'CDNFolder');
